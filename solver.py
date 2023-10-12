28bbeedc2f8deb72e26bcc8d60a0cd2682de9336
luoweiWHUT <1615108374@qq.com>
2023-10-12 18:34:39 +0800

    测试模型训练效果

--- solver.py
+++ solver.py
@@ -2,16 +2,20 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 """求解目标max(V)及对应的s"""
-N=12
+N = 12
+
 
 def v_compute(s):
     """根据赛题规则进行计算,价值函数"""
     return torch.sum(s)
     # raise NotImplementedError
+
+
 def update_s(s):
-    s= torch.rand(2,12,4)
-    s.requires_grad=True
+    s = torch.rand(2, 12, 4)
+    s.requires_grad = True
     return s
+
 
 class PolicyNet(torch.nn.Module):
     """策略网络,输入s,输出s下动作概率的分布
@@ -24,28 +28,43 @@ class PolicyNet(torch.nn.Module):
 
     def __init__(self, action_dim=3, in_channels=2):
         super(PolicyNet, self).__init__()
-        self.conv1 = torch.nn.Conv2d(2, 10,kernel_size=1, stride=1)
-        self.conv2 = torch.nn.Conv2d(10, 2, kernel_size=2, stride=1)
+        self.conv1 = torch.nn.Conv2d(2, 10, kernel_size=1, stride=1)
+        self.conv2 = torch.nn.Conv2d(10, 5, kernel_size=1, stride=1)
+        self.conv3 = torch.nn.Conv2d(5, 2, kernel_size=2, stride=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        print(x.shape)
         x = F.relu(self.conv2(x))
-        print(x.shape)
-        return F.softmax(x,dim=1)
+        x = F.relu(self.conv3(x))
+        return F.softmax(x, dim=1)
+        # return x
 
 
-s= torch.rand(2,12,4) # 输入状态:两行晶体管的宽度、left、mid、right
-s.requires_grad=True
+"""train"""
+s = torch.rand(2, 12, 4)  # 输入状态:两行晶体管的宽度、left、mid、right
+s.requires_grad = True
 answer = np.zeros([2, 12, 3])  # 输出结果：两行晶体管的位置、是否翻转
 model = PolicyNet()
-optimizer = torch.optim.Adam(model.parameters(),lr=0.001)  # 使用Adam优化器
-for iter in range(100):
-    optimizer.zero_grad()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)  # 使用Adam优化器
+optimizer.zero_grad()
+loss_sum = 0
+for iter in range(10000):
+    optimizer.zero_grad()#置零
     action = model(s)
-    s = update_s(s)
-    reward = torch.sum(s)
-    loss = 100-reward
+    reward = torch.sum(action)+torch.sum(s)
+    loss = torch.abs(100-reward)
     loss.backward()
     optimizer.step()  # 梯度下降
-    print(loss)
+
+    loss_sum += loss
+    s = update_s(s)
+    if iter % 100 == 0:
+        print(loss_sum/100)
+        loss_sum = 0
+torch.save(model, 'model.pt')
+""""""
+model = torch.load('model.pt')
+for i in range(100):
+    s = torch.rand(2, 12, 4)
+    ac = model(s)
+    print(torch.sum(ac)+torch.sum(s))
