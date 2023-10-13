a02e0079ae9bc52945841641d05332a058ba9b24
luoweiWHUT <1615108374@qq.com>
2023-10-13 19:56:41 +0800

    完善nets文件解析，将结果保存为对象列表mos_list和词数组words_list

--- parse.py
+++ parse.py
@@ -2,7 +2,8 @@ import re
 
 
 class Mos:
-    def __init__(self, name, type, left, mid, right, w, l):
+    __slots__=('name','type', 'left', 'mid', 'right', 'w', 'l',)
+    def __init__(self, name,left, mid, right, type, w, l):
         self.name=name
         self.left = left
         self.mid = mid
@@ -10,23 +11,28 @@ class Mos:
         self.type = type
         self.w = w
         self.l = l
-
-
-def parse(path):
-    """解析晶体管文件"""
-    mos_list = []
-    pattern = re.compile(
-        r"(.*\s)(.*\s)(.*\s)(.*\s)(.*\s).*\sW=(.*)u\sL=(.*)n\n")
-    with open(path, 'r') as f:
-        for line in f:
-            line_data = re.match(pattern, line)
-            if line_data:
-                name, left, mid, right, type, w, l = line_data.groups()
-                print(name, left, mid, right, type, float(w)*1000, float(l))
-                mos_list.append(Mos(name, left, mid, right,
-                                type, float(w)*1000, float(l)))
-    return mos_list
+class Parser:
+    def __init__(self) -> None:
+        self.path = None
+        self.mos_list = []
+        self.attributes = ('name','type', 'left', 'mid', 'right', 'w', 'l')
+        self.words_list=[]
+        self.pattern = re.compile(r"(.*\s)(.*\s)(.*\s)(.*\s)(.*\s).*\sW=(.*)u\sL=(.*)n\n")
+    def parse(self,path):
+        self.path = path
+        with open(self.path, 'r') as f:
+            for line in f:
+                line_data = re.match(self.pattern, line)
+                if line_data:
+                    name, left, mid, right, type, w, l = line_data.groups()
+                    print(name, left, mid, right, type, float(w)*1000, float(l))
+                    self.mos_list.append(Mos(name, left, mid, right,
+                            type, float(w)*1000, float(l)))
+                    self.words_list.append([name, left, mid, right, type, float(w)*1000, float(l)])
+        return self.mos_list,self.words_list
 
 
 if __name__ == "__main__":
-    mos_list = parse('test1.nets')
+    paser = Parser()
+    paser.pattern=re.compile(r"(.*\s)(.*\s)(.*\s)(.*\s)(.*\s).*\sW=(.*)u\sL=(.*)n\n")
+    mos_list,words_list = paser.parse('test1.nets')
