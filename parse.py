e78f587b3d6bc7f7a19faf3157327ffedcab8bd0
luoweiWHUT <1615108374@qq.com>
2023-11-03 14:22:18 +0800

    修复了文件解析的bug

--- parse.py
+++ parse.py
@@ -11,13 +11,15 @@ class Mos:
         self.type = type
         self.w = w
         self.l = l
+    def __str__(self):
+        return f"'name':{self.name},'y':{self.type}, 'source':{self.left},'gain':{self.mid}, 'drain':{self.right}, 'width:{self.w}'"
 class Parser:
     def __init__(self) -> None:
         self.path = None
         self.mos_list = []
         self.attributes = ('name','type', 'left', 'mid', 'right', 'w', 'l')
         self.words_list=[]
-        self.pattern = re.compile(r"(.*\s)(.*\s)(.*\s)(.*\s)(.*\s).*\sW=(.*)u\sL=(.*)n\n")
+        self.pattern = re.compile(r"(.*)\s(.*)\s(.*)\s(.*)\s(.*)\s.*\sW=(.*)u\sL=(.*)n\n")
     def parse(self,path):
         self.path = path
         with open(self.path, 'r') as f:
@@ -25,10 +27,9 @@ class Parser:
                 line_data = re.match(self.pattern, line)
                 if line_data:
                     name, left, mid, right, type, w, l = line_data.groups()
-                    print(name, left, mid, right, type, float(w)*1000, float(l))
-                    self.mos_list.append(Mos(name, left, mid, right,
-                            type, float(w)*1000, float(l)))
-                    self.words_list.append([name, left, mid, right, type, float(w)*1000, float(l)])
+                    params = [name, left, mid, right, type, int(float(w)*1000), int(float(l))]
+                    self.mos_list.append(Mos(*params))
+                    self.words_list.append(params)
         return self.mos_list,self.words_list
 
 
