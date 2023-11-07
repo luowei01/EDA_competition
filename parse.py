ad032b366a2e07bcc139c23101cfffdb9511f774
luoweiWHUT <1615108374@qq.com>
2023-11-07 14:08:13 +0800

    完善了数据集行尾有空格解析错误的bug

--- parse.py
+++ parse.py
@@ -11,15 +11,16 @@ class Mos:
         self.type = type
         self.w = w
         self.l = l
-    def __str__(self):
-        return f"'name':{self.name},'y':{self.type}, 'source':{self.left},'gain':{self.mid}, 'drain':{self.right}, 'width:{self.w}'"
+    # def __str__(self):
+    def __repr__(self):
+        return f"'name':{self.name},'y':{self.type}, 'source':{self.left},'gain':{self.mid}, 'drain':{self.right}, 'width:{self.w}'\n"
 class Parser:
     def __init__(self) -> None:
         self.path = None
         self.mos_list = []
         self.attributes = ('name','type', 'left', 'mid', 'right', 'w', 'l')
         self.words_list=[]
-        self.pattern = re.compile(r"(.*)\s(.*)\s(.*)\s(.*)\s(.*)\s.*\sW=(.*)u\sL=(.*)n\n")
+        self.pattern = re.compile(r"(.*)\s(.*)\s(.*)\s(.*)\s(.*)\s.*\s\w=(.*)\sL=(.*)n\s?\n")
     def parse(self,path):
         self.path = path
         with open(self.path, 'r') as f:
@@ -27,7 +28,7 @@ class Parser:
                 line_data = re.match(self.pattern, line)
                 if line_data:
                     name, left, mid, right, type, w, l = line_data.groups()
-                    params = [name, left, mid, right, type, int(float(w)*1000), int(float(l))]
+                    params = [name, left, mid, right, type, int(float(w[:-1])*1000) if w[-1]=='u' else int(float(w[:-1])), int(float(l))]
                     self.mos_list.append(Mos(*params))
                     self.words_list.append(params)
         return self.mos_list,self.words_list
@@ -35,6 +36,6 @@ class Parser:
 
 if __name__ == "__main__":
     paser = Parser()
-    paser.pattern=re.compile(r"(.*\s)(.*\s)(.*\s)(.*\s)(.*\s).*\sW=(.*)u\sL=(.*)n\n")
     # mos_list,words_list = paser.parse('public/cells.spi')
-    mos_list,words_list = paser.parse('test.txt')
+    mos_list,words_list = paser.parse('test_case\cells.spi')
+    print(len(mos_list))
